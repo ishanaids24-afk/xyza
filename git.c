6caed71f2e3df9c33781c1f990bb614615152933
goodboy8181 <goli.aids24@cmrit.ac.in>
2025-10-28 11:01:32 +0530

    Add randomized quicksort implementation in git.c
    Implemented randomized quicksort algorithm with partitioning.

--- git.c
+++ git.c
@@ -1,1 +1,49 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
 
+void swap(int* a, int* b) {
+    int t = *a;
+    *a = *b;
+    *b = t;
+}
+
+int partition(int arr[], int low, int high) {
+    int pivot = arr[high];
+    int i = (low - 1);
+    for (int j = low; j <= high - 1; j++) {
+        if (arr[j] < pivot) {
+            i++;
+            swap(&arr[i], &arr[j]);
+        }
+    }
+    swap(&arr[i + 1], &arr[high]);
+    return (i + 1);
+}
+
+int randomizedPartition(int arr[], int low, int high) {
+    srand(time(NULL)); // Seed the random number generator
+    int random = low + rand() % (high - low + 1); // Generate random index
+    swap(&arr[random], &arr[high]); // Swap random element with last element
+    return partition(arr, low, high);
+}
+
+void randomizedQuickSort(int arr[], int low, int high) {
+    if (low < high) {
+        int pi = randomizedPartition(arr, low, high);
+        randomizedQuickSort(arr, low, pi - 1);
+        randomizedQuickSort(arr, pi + 1, high);
+    }
+}
+
+int main() {
+    int arr[] = {10, 7, 8, 9, 1, 5};
+    int n = sizeof(arr) / sizeof(arr[0]);
+    randomizedQuickSort(arr, 0, n - 1);
+    printf("Sorted array: ");
+    for (int i = 0; i < n; i++) {
+        printf("%d ", arr[i]);
+    }
+    printf("\n");
+    return 0;
+}
